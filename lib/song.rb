2bd5fa950046224af4c1a1208f1a2db88a692cfd
Brett Weisberg <weisberg.brett@gmail.com>
2018-04-23 18:32:11 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -62,7 +62,12 @@ attr_reader :genre, :artist
           self.create(name)
   end
 end
+def new_from_filename(name)
+  artist, song, genre = name.split(' - ')
+  artist = Artist.find_or_create_by_name(artist)
+  genre = Genre.find_or_create_by_name(genre)
+  new(song, artist, genre)
+end
 
-  
 
 end
