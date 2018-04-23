65d359dcbb8c4eed3f282893bc32179811e0a921
Brett Weisberg <weisberg.brett@gmail.com>
2018-04-23 18:27:39 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -56,11 +56,14 @@ attr_reader :genre, :artist
     end
 
   def self.find_or_create_by_name(name)
-    if self.find_by_name(name) != nil 
+    if self.find_by_name(name) != nil
           return self.find_by_name(name)
         else
           self.create(name)
   end
 end
+
+  def new_from_filename(name)
+
 
 end
