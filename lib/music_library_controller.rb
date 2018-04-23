d27a2ce0fdbb73e080a703c9d973188a3dd9bbdd
Brett Weisberg <weisberg.brett@gmail.com>
2018-04-23 19:53:38 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -32,7 +32,7 @@ class MusicLibraryController
 
   def list_artists
 
-    Artist.all.sort_by(&:name).each.with_index(1) do |art, idx|
+    Artist.all.sort_by {|artistname| artistname.name }.each.with_index(1) do |art, idx|
         #binding.pry
       puts "#{idx}. #{art.name}"
     end
