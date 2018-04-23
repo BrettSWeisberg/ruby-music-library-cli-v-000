4d0549125bf4761c4d61614b1b20a88313cbdb3f
Brett Weisberg <weisberg.brett@gmail.com>
2018-04-23 19:19:10 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -23,4 +23,11 @@ class MusicLibraryController
     input = gets.strip
   end
   end
+
+  def list_songs
+    Song.all.sort_by(&:name).each.with_index(1) do |song, idx|
+      puts "#{idx} #{song.artist.name} - #{song.name} -#{song.genre.name}"
+    end
+  end
+
 end
